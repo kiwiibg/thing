bd512c5da932d8637a76ffecaab5fe5889edf191
d-beezee <59012086+d-beezee@users.noreply.github.com>
2022-04-02 19:18:44 +0200

    Update sync_template.py

--- .github/workflows/sync_template.py
+++ .github/workflows/sync_template.py
@@ -10,20 +10,33 @@ response = requests.get("https://raw.githubusercontent.com/italyplace/rplace/mai
 img = Image.open(BytesIO(response.content))
 img = img.resize((img.size[0] * 3, img.size[1] * 3), Image.NEAREST)
 
-mask_url = "https://media.discordapp.net/attachments/267492253168173056/959625681141104700/mask.png"
+response2 = requests.get("https://raw.githubusercontent.com/italyplace/rplace/main/art.png?=" + str(ts))
+img2 = Image.open(BytesIO(response.content))
+img2 = img2.resize((img.size[0] * 3, img.size[1] * 3), Image.NEAREST)
+
+mask_url = "https://raw.githubusercontent.com/italyplace/thing/raw/main/mask2x.png"
 response = requests.get(mask_url)
 mask_i = Image.open(BytesIO(response.content))
 #mask_i = ImageOps.invert(mask_i.convert('RGB'))
 mask_i.save("mask_i.png")
-mask = Image.new("1", (3000, 3000), 0)
+mask = Image.new("1", (6000, 3000), 0)
 mask.paste(mask_i)
 mask.save("mask.png")
 
 tl = (782 * 3, 254  * 3) # top left corner
 
-final_img = Image.new('RGBA', (3000, 3000))
-unmasked_img = Image.new('RGBA', (3000, 3000))
+final_img = Image.new('RGBA', (6000, 3000))
+unmasked_img = Image.new('RGBA', (6000, 3000))
 unmasked_img.paste(img, tl)
 final_img = Image.composite(final_img, unmasked_img, mask)
 final_img.save("template.png")
+
+tl2 = (1710 * 3, 516  * 3) # top left corner
+
+final_img2 = Image.new('RGBA', (6000, 3000))
+unmasked_img2 = Image.new('RGBA', (6000, 3000))
+unmasked_img2.paste(img2, tl2)
+final_img = Image.composite(final_img, unmasked_img2, mask)
+
+final_img.save("template2x.png")
 
