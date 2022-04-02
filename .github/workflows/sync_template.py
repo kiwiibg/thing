0449927b47d18a1324feefca64562faadd992f93
d-beezee <59012086+d-beezee@users.noreply.github.com>
2022-04-02 13:59:14 +0200

    Update sync_template.py

--- .github/workflows/sync_template.py
+++ .github/workflows/sync_template.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageOps
 import requests
 from io import BytesIO
 
-response = requests.get("https://raw.githubusercontent.com/italyplace/thing/main/art.png")
+response = requests.get("https://raw.githubusercontent.com/italyplace/rplace/main/art.png")
 img = Image.open(BytesIO(response.content))
 img = img.resize((img.size[0] * 3, img.size[1] * 3), Image.NEAREST)
 
