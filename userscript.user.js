9ac455f29247f5b1a353896d25d279d490ef2656
d-beezee <59012086+d-beezee@users.noreply.github.com>
2022-04-02 18:07:39 +0200

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ritaly template
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the canvas!
 // @author       oralekin
 // @match        https://hot-potato.reddit.com/embed*
@@ -12,8 +12,11 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
+            if (!Date.now) {
+                Date.now = function() { return new Date().getTime(); }
+            }
             const i = document.createElement("img");
-            i.src = "https://github.com/italyplace/thing/raw/main/template.png";
+            i.src = "https://github.com/italyplace/thing/raw/main/template.png?" + Date.now();
             i.style = "position: absolute;left: 0;top: 0px;image-rendering: pixelated;width: 1000px;height: 1000px;";
             i.id = "mcss-overlay"
             i.setAttribute("vis",1)
