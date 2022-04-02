501b28fa89470406c4c989dfb46d9fcbed3de3ed
d-beezee <59012086+d-beezee@users.noreply.github.com>
2022-04-02 19:30:58 +0200

    Update sync_template.py

--- .github/workflows/sync_template.py
+++ .github/workflows/sync_template.py
@@ -35,8 +35,8 @@ tl2 = (1710 * 3, 516  * 3) # top left corner
 
 final_img2 = Image.new('RGBA', (6000, 3000))
 unmasked_img2 = Image.new('RGBA', (6000, 3000))
+unmasked_img2.paste(img, tl)
 unmasked_img2.paste(img2, tl2)
-final_img2 = Image.composite(final_img2, unmasked_img, mask)
 final_img2 = Image.composite(final_img2, unmasked_img2, mask)
 
 final_img2.save("template2x.png")
