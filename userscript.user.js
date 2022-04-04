2393668d566212077d400719782fbcd39dc5db61
kiwiibg <peca7a17@gmail.com>
2022-04-04 05:56:34 +0300

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
                 Date.now = function() { return new Date().getTime(); }
             }
             const i = document.createElement("img");
-            i.src = "https://github.com/italyplace/thing/raw/main/template.png?" + Date.now();
+            i.src = "https://github.com/kiwiibg/thing/raw/main/output.png?" + Date.now();
             i.style = "position: absolute;left: 0;top: 0px;image-rendering: pixelated;width: 2000px;height: 2000px;";
             i.id = "mcss-overlay"
             i.setAttribute("vis",1)
